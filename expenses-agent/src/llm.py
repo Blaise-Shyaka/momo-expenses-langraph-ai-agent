b22f205c8f0219a132ed16f7235aa2a34c16c253
Blaise Shyaka <blaiseshyaka95@gmail.com>
2025-11-04 18:44:30 +0200

    Removed unused types

--- expenses-agent/src/llm.py
+++ expenses-agent/src/llm.py
@@ -3,7 +3,7 @@ from langchain_core.messages import SystemMessage, AIMessage
 from .tools import tools
 from datetime import datetime
 
-gemini_llm = ChatGoogleGenerativeAI(model="gemini-2.0-flash", temperature=0.5).bind_tools(tools) # type: ignore - Pylance has a hard time handling these types - similar to here: https://github.com/langchain-ai/langchain/issues/33019
+gemini_llm = ChatGoogleGenerativeAI(model="gemini-2.0-flash", temperature=0.5).bind_tools(tools)
 system_message = SystemMessage(content=f"Today's date is {datetime.today()} You are an AI assistant tasked with being and expenses tracker for users. Your name is Reddington. You have a friendly, helpful, playful, and respectful tone.")
 llm_message = AIMessage(content="Hello, I'm Reddington. I will assist you with recording your expenses. How can I help you today?")
 messages = [system_message, llm_message]
